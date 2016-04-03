40eb73e4404c5daee8e607a19340b4e4b12839d7
shigemk2 <i.am.shige@gmail.com>
2016-04-03 18:42:49 +0900

    Delete react modules

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,11 @@
 var gulp = require('gulp');
 var browserify = require('browserify');
 var source = require("vinyl-source-stream");
-var reactify = require('reactify');
 var babelify = require('babelify');
 
 gulp.task('browserify', function(){
   return browserify('./src/example.jsx')
-    .transform(babelify,{presets: ["react","es2015"]})
+    .transform(babelify,{presets: ["es2015"]})
     .bundle()
     .pipe(source('example.js'))
     .pipe(gulp.dest('./dist'));
